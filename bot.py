ad57eae6932d7be85b349b41d76d1fe1d03a64f5
yoonjae <yoonbal0406@naver.com>
2021-09-02 15:49:27 +0900

    first commit

---
+++ bot.py
@@ -0,0 +1,88 @@
+import discord
+import asyncio
+from discord.ext import commands
+import crawl
+import numpy as np
+import pandas as pd
+
+token = "ODgyMTYyNzQ5NTM3Mzk0NzE4.YS3YFQ.Qa1I76p2kzOBiezinAbnGqKth_Y"
+bot_activity = discord.Game(name='동현우 감시')
+bot = commands.Bot(command_prefix='.',activity = bot_activity)
+
+
+
+
+@bot.event
+async def on_ready():
+    print('등장!')
+
+@bot.command()
+async def 핑(ctx):
+    await ctx.send('퐁')
+
+@bot.event
+async def on_message(message):
+    message_content = message.content
+    if message_content == '김기수':
+        await message.channel.send('존잘')
+        # await message.delete()
+    elif message_content == '고윤재':
+        await message.channel.send('존잘')
+    elif message_content == '최민서':
+        await message.channel.send('존잘')
+    elif message_content == '배':
+        await message.channel.send('수찬')
+    elif message_content == '남필량':
+        await message.channel.send(':nose:')
+    elif message_content == '박태욱':
+        await message.channel.send('겨털')
+    elif message_content == '씨발':
+        await message.channel.send('바르고 고운말을 사용합시다')
+        await message.delete()
+
+
+    await bot.process_commands(message)
+
+@bot.command()
+async def message1(ctx,context):
+    await ctx.send(f"단일명령어내용 : {context}\n 현재채널ID : {ctx.channel.id}\n 현재채널이름 : {ctx.channel.name}")
+
+@bot.command()
+async def 따라하기(ctx, *, text):
+    await ctx.send(text)
+
+@bot.command()
+async def 전적(ctx, *, text):
+    await ctx.send(f"{text}님의 최근 전적 불러오는중")
+    await ctx.send(crawl.gamelog(text))
+
+@bot.command()
+async def 티어(ctx, *, text):
+    soltier,subtier = crawl.nowtier(text).replace(('Bronze','브론즈'),('Silver','실버'))
+    await ctx.send(f"{text}님의 현재 티어\n솔로랭크 : {soltier}\n자유랭크 : {subtier}")
+    print(soltier,subtier)
+
+@bot.command()
+async def 도움말(ctx):
+    await ctx.send('.핑 : 동모니터가 퐁 으로 받아줍니다.')
+    await ctx.send('.전적 [롤닉네임] : 해당 사용자의 최근 20판의 전적을 보여줍니다.')
+    await ctx.send('.티어 [롤닉네임] : 해당 사용자의 현재 솔랭 티어를 보여줍니다')
+    await ctx.send('.따라하기 [따라할말] : 입력한 말을 따라합니다.')
+
+
+bot.run(token)
+
+
+
+
+
+
+
+
+# import discord
+
+# client = discord.Client()
+
+# token = "ODgyMTYyNzQ5NTM3Mzk0NzE4.YS3YFQ.Qa1I76p2kzOBiezinAbnGqKth_Y"
+
+# client.run(token)
---
+++ crawl.py
@@ -0,0 +1,143 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+# from webdriver_manager.chrome import ChromeDriverManager
+import time
+from selenium.webdriver.common.alert import Alert
+import requests
+import numpy as np
+import pandas as pd
+from datetime import datetime
+from tabulate import tabulate
+
+
+
+
+
+options = webdriver.ChromeOptions()
+# 창 숨기는 옵션 추가
+options.add_argument("headless")
+driver = webdriver.Chrome("/Users/goyoonjae/Desktop/dongmonitor/chromedriver",options=options)
+# driver = webdriver.Chrome("/Users/goyoonjae/Desktop/dongmonitor/chromedriver")
+
+def gamelog(nickname):
+
+    driver = webdriver.Chrome("/Users/goyoonjae/Desktop/dongmonitor/chromedriver",options=options)
+    url = 'https://www.op.gg/summoner/userName='
+    # inp = input('롤 닉넴 입력 : ')
+    url = url+nickname
+    driver.get(url)
+    alert = Alert(driver)
+    req = requests.get(url)
+    html = req.text
+    soup = BeautifulSoup(html,'html.parser')
+    pd.set_option('expand_frame_repr',False)
+
+    driver.find_element_by_css_selector('#SummonerRefreshButton').click()
+
+    try:
+        # time.sleep(3)
+        alert = Alert(driver)
+        # alert = driver.switch_to.alert
+        
+        print(alert)
+        time.sleep(1)
+        print('alert detected!')
+        alert.accept()
+        time.sleep(1)
+        alert.accept()
+    except:
+        "noal"
+        # time.sleep(3)
+        # print('no alert!')
+    # kda = soup.select('.KDA .KDA span')
+
+    kda = soup.select('.GameItem>.Content>.KDA>.KDA>span')
+    kdas = []
+    for tag in kda:
+        kdas.append(tag.get_text().strip())
+    KDA = np.reshape(kdas,(20,3))
+    print('kda 조회중...')
+    # print(KDA)
+
+
+    gameresult = soup.select('.GameStats>.GameResult')
+    gameresults = []
+    for tag in gameresult:
+        gameresults.append(tag.get_text().strip())
+    GRES = np.reshape(gameresults,(20,1))
+    print('게임결과 조회중...')
+    # print(GRES)
+
+
+    champion = soup.select('.Content>.GameSettingInfo>.ChampionName>a')
+    champions = []
+    for tag in champion:
+        champions.append(tag.get_text())
+    CHAMP = np.reshape(champions,(20,1))
+    print('챔피언 조회중...')
+
+    # for tag in champion:
+    #     print(tag.get_text())
+
+    data = pd.DataFrame(CHAMP)
+    subdf = pd.DataFrame(GRES)
+    data = pd.concat([data, subdf],axis = 1)
+    subdf = pd.DataFrame(KDA)
+    data = pd.concat([data,subdf],axis = 1)
+    data.columns = ['CHAMP','RESULT','KILL','DEATH','ASSIST']
+    data = tabulate(data,headers='keys',tablefmt='pretty',showindex=range(1,21))
+    
+    # print(data)
+    # for tag in kda:
+    #     print(tag.get_text())
+    driver.delete_all_cookies()
+    driver.quit()
+    print('정보반환완료')
+    print(data)
+    print(type(data))
+    return data
+    
+
+def nowtier(nickname):
+    driver = webdriver.Chrome("/Users/goyoonjae/Desktop/dongmonitor/chromedriver",options=options)
+    url = 'https://www.op.gg/summoner/userName='
+    # inp = input('롤 닉넴 입력 : ')
+    url = url+nickname
+    driver.get(url)
+    alert = Alert(driver)
+    req = requests.get(url)
+    html = req.text
+    soup = BeautifulSoup(html,'html.parser')
+
+    driver.find_element_by_css_selector('#SummonerRefreshButton').click()
+
+    try:
+        # time.sleep(3)
+        alert = Alert(driver)
+        # alert = driver.switch_to.alert
+        
+        print(alert)
+        # time.sleep(1)
+        print('alert detected!')
+        alert.accept()
+        # time.sleep(1)
+        alert.accept()
+    except:
+        "noal"
+        # time.sleep(3)
+        # print('no alert!')
+
+    
+    soltier = soup.find(class_= 'TierRank').get_text()
+    subtier = soup.find(class_="sub-tier__rank-tier").get_text().strip()
+    driver.delete_all_cookies()
+    driver.quit()
+    return soltier, subtier
+
+
+
+
+    # kda = soup.select('.KDA .KDA span')
+
+# inp = input('닉네임입력')
+# crawler(inp)
